5d7d36e8f686b04324c7e2ac4a75258b7e04ae65
Alexander Blagoev <alexander.i.blagoev@gmail.com>
2022-01-16 20:26:45 +0200

    Initial project functionality

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::middleware(['auth'])->group(function () {
+    Route::get('/api/users/{user}/activity', [UserController::class, 'activity']);
+});
